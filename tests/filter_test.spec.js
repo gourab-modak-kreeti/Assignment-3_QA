21cb5f1e3af695e2228f2438cb89dde27eddd549
Gourab Modak <gourab.modak@kawach.ai>
2026-08-24 15:53:03 +0530

    Added test for filters tried storing snapshots seperately

--- tests/filter_test.spec.js
+++ tests/filter_test.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+// i am sitll working on this
 import { Login } from "../pages/login.js";
 import { Products } from "../pages/products.js";
 import { Cart } from "../pages/cart.js";
